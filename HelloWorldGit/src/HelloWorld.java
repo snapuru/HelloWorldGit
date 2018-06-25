c7062f183de9921fac56b149ff7dd698627fb85f
Damien <Damien@DAMIEN-PC>
2018-06-24 22:09:06 -0300

    Initial Commit of HelloWorldTool

---
+++ HelloWorldGit/src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Usuario user = new Usuario("Damien", "Bourel");
+		
+		System.out.println(user.getNombre());
+		System.out.println(user.getApellido());
+	}
+
+}
